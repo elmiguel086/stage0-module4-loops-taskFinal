06660c2e3e4647d03e5a97cb60e6f7e204be438f
elmiguel0 <miguelangel.ramos@ibm.com>
2022-12-08 19:35:38 -0600

    changes done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1 + " x " + numberTableToPrint
+                    + " = " + (i+1) * numberTableToPrint);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,23 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int a;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength-i; j++) {
+                System.out.print(" ");
+            }
+            a=i;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a--);
+            }
+            a=2;
+            if(i>=2){
+                for (int j = 1; j <= i-1; j++) {
+                    System.out.print(a++);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
